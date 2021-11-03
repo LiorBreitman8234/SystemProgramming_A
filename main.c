0d293759f234a648fabb05053c914622d496fee6
Lior Breitman <liorbreitman321@gmail.com>
2021-11-03 15:46:06 +0200

    changed main

--- main.c
+++ main.c
@@ -4,9 +4,7 @@
 int main(){
     int num1 =0;
     int num2 = 0;
-    printf("Enter the first number:");
     scanf("%d",&num1);
-    printf("Enter another number:");
     scanf("%d", &num2);
     if(num1 > num2)
     {
@@ -14,42 +12,42 @@ int main(){
         num1 = num2;
         num2 = temp;
     }
-    printf("The Prime numbers are: ");
+    printf("The Prime numbers are:");
     for(int i = num1; i <= num2; i++)
     {
         int checkPrime = isPrime(i);
         if(checkPrime)
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\n The Armstrong numbers are: ");
+    printf("\nThe Armstrong numbers are:");
     for(int i = num1; i <= num2; i++)
     {
         int checkArmStrong = isArmstrong(i);
         if(checkArmStrong)
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\n The Strong numbers are: ");
+    printf("\nThe Strong numbers are:");
     for(int i = num1; i <= num2; i++)
     {
         int checkStrong = isStrong(i);
         if(checkStrong)
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\n The Palindrome numbers are: ");
+    printf("\nThe Palindrome numbers are:");
     for(int i = num1; i <= num2; i++)
     {
         int checkPalindrome = isPalindrome(i);
         if(checkPalindrome)
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\n");
+
     return 0;
 }
